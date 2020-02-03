cbfc49e5e711095732e6e05a6c1439be26f02540
AndreaH <805133@apps.district196.org>
2020-02-03 07:58:16 -0600

    Initial Commit

---
+++ theRealProjectp2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  theRealProjectp2
+//
+//  Created by HAGAPE, ANDREA on 2/3/20.
+//  Copyright © 2020 HAGAPE, ANDREA. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ theRealProjectp2Tests/theRealProjectp2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  theRealProjectp2Tests.swift
+//  theRealProjectp2Tests
+//
+//  Created by HAGAPE, ANDREA on 2/3/20.
+//  Copyright © 2020 HAGAPE, ANDREA. All rights reserved.
+//
+
+import XCTest
+@testable import theRealProjectp2
+
+class theRealProjectp2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
