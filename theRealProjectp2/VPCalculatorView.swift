a06e705f9598c8772e35aea944df5c3670528062
ranishah02 <58517935+ranishah02@users.noreply.github.com>
2020-03-11 08:06:38 -0500

    3/11/2020 Rani 8:06

--- theRealProjectp2/VPCalculatorView.swift
+++ theRealProjectp2/VPCalculatorView.swift
@@ -10,12 +10,37 @@ import SwiftUI
  
 struct VPCalculatorView: View {
     @Binding var item: ListItem
+    @State var s1pitch: String = ""
+    @State var s1tempo: String = ""
+    @State var s1sound: String = ""
+    
+    @State var s2pitch: String = ""
+    @State var s2tempo: String = ""
+    @State var s2sound: String = ""
  
     var body: some View {
+        VStack{
         TextField("", text: self.$item.name)
+            HStack{
+                List{Text("hello"); Text("Rhythm"); Text("Pitch"); Text("Tempo"); Text("Dynamic"); Text("Sound")}
+                
+                List{Text("Scale 1"); Text("n/a"); TextField("/20", text: $s1pitch); TextField("/20", text: $s1tempo); Text("n/a"); TextField("/20", text: $s1sound)}
+                
+            List{Text("Scale 2"); Text("n/a"); TextField("/20", text: $s2pitch); TextField("/20", text: $s2tempo); Text("n/a"); TextField("/20", text: $s2sound)}
+            List{Text("Scale 3")}
+            List{Text("Snare Etude")}
+            List{Text("Mallet Etude")}
+            List{Text("Timp Etude")}
+            List{Text("Snare Sight Read")}
+            List{Text("Mallet Sight Read")}
+            
+        }
  
-    
+        }
     }
+}
+struct VPCalc{
+    
 }
  
  
