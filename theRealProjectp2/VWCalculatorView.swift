7df7274c08ebcf4f6eb80db2da266073de63bac0
Rani Shah <837283@apps.district196.org>
2020-04-07 15:03:42 -0500

    4/7/20 3:00 rani, worked on Wind Calculator a bit

--- theRealProjectp2/VWCalculatorView.swift
+++ theRealProjectp2/VWCalculatorView.swift
@@ -11,6 +11,55 @@ import SwiftUI
 struct VWCalculatorView: View {
     @Binding var item: ListItem
     @State var comment: String = ""
+    
+    @State var s1rhythm: String = ""
+    @State var s1pitch: String = ""
+    @State var s1tempo: String = ""
+    @State var s1dynamic: String = ""
+    @State var s1sound: String = ""
+    
+    @State var s2rhythm: String = ""
+    @State var s2pitch: String = ""
+    @State var s2tempo: String = ""
+    @State var s2dynamic: String = ""
+    @State var s2sound: String = ""
+    
+    @State var s3rhythm: String = ""
+    @State var s3pitch: String = ""
+    @State var s3tempo: String = ""
+    @State var s3dynamic: String = ""
+    @State var s3sound: String = ""
+    
+    @State var e1rhythm: String = ""
+    @State var e1pitch: String = ""
+    @State var e1tempo: String = ""
+    @State var e1dynamic: String = ""
+    @State var e1sound: String = ""
+    
+    @State var e2rhythm: String = ""
+    @State var e2pitch: String = ""
+    @State var e2tempo: String = ""
+    @State var e2dynamic: String = ""
+    @State var e2sound: String = ""
+    
+    @State var e3rhythm: String = ""
+    @State var e3pitch: String = ""
+    @State var e3tempo: String = ""
+    @State var e3dynamic: String = ""
+    @State var e3sound: String = ""
+    
+    @State var sr1rhythm: String = ""
+    @State var sr1pitch: String = ""
+    @State var sr1tempo: String = ""
+    @State var sr1dynamic: String = ""
+    @State var sr1sound: String = ""
+    
+    @State var sr2rhythm: String = ""
+    @State var sr2pitch: String = ""
+    @State var sr2tempo: String = ""
+    @State var sr2dynamic: String = ""
+    @State var sr2sound: String = ""
+    
     var body: some View {
         VStack{
             HStack{
@@ -24,7 +73,7 @@ struct VWCalculatorView: View {
             Spacer()
 
             HStack{
-            List{Text("hello")}
+            List{Text(""); Text("Rhythm"); Text("Pitch"); Text("Tempo"); Text("Dynamic"); Text("Sound")}
             List{Text("hello")}
             List{Text("hello")}
             List{Text("hello")}
